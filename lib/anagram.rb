ab43311adc8e633c111941ed96df29ca7be59ef3
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-19 21:35:05 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -13,11 +13,11 @@ class Anagram
     matches = []
     test = words_array.map { |w| w.split("") }
     test = test.map { |l| l.sort }
-binding.pry
+#binding.pry
       test.map do |t|
 
         if t == word_sorted
-          #binding.pry
+          binding.pry
           matches << words_array[test.find_index(t)]
         end
       end
