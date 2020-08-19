db2c75a0810d8b9cabaa6ccc43c0883690cea1ab
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-19 20:15:51 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,6 +7,9 @@ class Anagram
   end
 
   def match(test)
+
+    test = test.map { |w| w.split("") }
+    test = test.map { |l| l.sort }
     word_sorted = @word.split("").sort
     test_sorted = test.split("").sort
     if word_sorted == test_sorted
