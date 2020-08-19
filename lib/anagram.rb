3e996a47fc4ec04bb632b86f13a3c939659c7551
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-19 20:17:32 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,12 +11,12 @@ class Anagram
     test = test.map { |w| w.split("") }
     test = test.map { |l| l.sort }
     word_sorted = @word.split("").sort
-    test_sorted = test.split("").sort
-    if word_sorted == test_sorted
-      return test
-    else
-      return nil
-    end
+
+    # if word_sorted == test_sorted
+    #   return test
+    # else
+    #   return nil
+    # end
   end
 
 end
