6e078bd330ec721cb21c0af2ffc1f2e5d2687bba
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-19 21:43:35 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,32 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words_array)
+
+    words_array.select { |word| word.split("").sort == @word.split("").sort }
+
+    # word_sorted = @word.split("").sort
+    # matches = []
+    # test = words_array.map { |w| w.split("") }
+    # test = test.map { |l| l.sort }
+    #
+    #   test.map do |t|
+    #
+    #     if t == word_sorted
+    #       binding.pry
+    #       matches << words_array[test.find_index(t)]
+    #     end
+    #   end
+    # matches
+
+  end
+
+
+
+end
