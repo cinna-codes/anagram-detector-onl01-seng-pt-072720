37d9615ecc19d0f4d2d390446f70d1feeabbd410
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-19 21:40:41 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,19 +9,20 @@ class Anagram
 
   def match(words_array)
 
-    word_sorted = @word.split("").sort
-    matches = []
-    test = words_array.map { |w| w.split("") }
-    test = test.map { |l| l.sort }
-#binding.pry
-      test.map do |t|
+    # word_sorted = @word.split("").sort
+    # matches = []
+    # test = words_array.map { |w| w.split("") }
+    # test = test.map { |l| l.sort }
+    #
+    #   test.map do |t|
+    #
+    #     if t == word_sorted
+    #       binding.pry
+    #       matches << words_array[test.find_index(t)]
+    #     end
+    #   end
+    # matches
 
-        if t == word_sorted
-          binding.pry
-          matches << words_array[test.find_index(t)]
-        end
-      end
-    matches
   end
 
 
